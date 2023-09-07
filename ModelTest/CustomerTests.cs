e863c54430b35659045d60fe9905c0acf6aa5aa1
Annie Tran <ngoc.tran@hope.edu>
2023-09-07 17:22:14 -0400

    Working on Movie and Customer tests

--- ModelTest/CustomerTests.cs
+++ ModelTest/CustomerTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Model;
+using System.Net.Mail;
 
 namespace ModelTest
 {
@@ -21,6 +22,75 @@ namespace ModelTest
 
             Assert.IsNotNull(customer.PreferredStores);
             Assert.AreEqual(0, customer.PreferredStores.Count);
+            Assert.IsNotNull(customer.CommunicationTypes);
+            Assert.AreEqual(0, customer.CommunicationTypes.Count);
+            Assert.IsNotNull(customer.Rentals);
+            Assert.AreEqual(0, customer.Rentals.Count);
+            Assert.IsNotNull(customer.LateRentals);
+            Assert.AreEqual(0, customer.LateRentals.Count);
+        }
+
+        [TestMethod]
+        public void ShouldEqualsWhenEmailAddrEqual() {
+            Customer customer = new Customer()
+            {
+                EmailAddress = "annie@gmail.com"
+
+            };
+
+            Customer comparison = new Customer()
+            {
+                EmailAddress = "annie@gmail.com"
+
+            };
+
+            Assert.AreEqual(true, customer.Equals(comparison));
+        }
+
+        [TestMethod]
+        public void ShouldNotEqualsWhenEmailAddrNotEqual()
+        {
+            Customer customer = new Customer()
+            {
+                EmailAddress = "ngoc@gmail.com"
+
+            };
+
+            Customer comparison = new Customer()
+            {
+                EmailAddress = "annie@gmail.com"
+
+            };
+
+            Assert.AreEqual(false, customer.Equals(comparison));
+        }
+
+        [TestMethod]
+        public void TestGetHashCode()
+        {
+            Customer customer = new Customer();
+            Assert.IsNotNull(customer.GetHashCode());
+        }
+
+        [TestMethod]
+        public void TestPassword()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestFullName()
+        {
+            Customer customer = new Customer()
+            {
+                Name = new Name()
+                {
+                    First = "Stu",
+                    Last = "Dent"
+                }
+            };
+
+            Assert.AreEqual("Stu Dent", customer.FullName);
         }
     }
 }
---
+++ ModelTest/MovieTests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Model;
+
+namespace ModelTest
+{
+    [TestClass]
+    public class MovieTests
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Movie movie = new Movie();
+
+            Assert.IsNotNull(movie.Reservations);
+            Assert.AreEqual(0, movie.Reservations.Count);
+            Assert.IsNotNull(movie.Genres);
+            Assert.AreEqual(0, movie.Genres.Count);
+            Assert.IsNotNull(movie.Roles);
+            Assert.AreEqual(0, movie.Roles.Count);
+        }
+
+        [TestMethod]
+        public void ShouldEqualsWhenTitleAndYearEqual()
+        {
+            Movie movie = new Movie()
+            {
+                Title = "Red",
+                Year = 2023
+            };
+
+            Movie comparison = new Movie()
+            {
+                Title = "Red",
+                Year = 2023
+            };
+
+            Assert.AreEqual(true, movie.Equals(comparison));
+        }
+
+        [TestMethod]
+        public void ShouldNotEqualsWhenTitleAndYearNotEqual()
+        {
+            Movie movie = new Movie()
+            {
+                Title = "Blue",
+                Year = 2023
+
+            };
+
+            Movie comparison = new Movie()
+            {
+                Title = "Red",
+                Year = 2023
+
+            };
+
+            Assert.AreEqual(false, movie.Equals(comparison));
+        }
+
+        [TestMethod]
+        public void TestGetHashCode()
+        {
+            Movie movie = new Movie();
+            Assert.IsNotNull(movie.GetHashCode());
+        }
+    }
+}
