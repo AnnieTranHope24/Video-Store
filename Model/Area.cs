e73e1f8dcd85e22d8fb1c1b8ed16b7f83b35099c
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 22:04:58 -0400

    Implemented Area

--- Model/Area.cs
+++ Model/Area.cs
@@ -8,5 +8,42 @@ namespace Model
 {
     public class Area
     {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual ISet<ZipCode> ZipCodes { get; protected internal set; }
+
+        public Area()
+        {
+            ZipCodes = new HashSet<ZipCode>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Area comparison = (Area)obj;
+                return (String.Equals(Name, comparison.Name));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public virtual void AddZipCode(ZipCode zipCode)
+        {
+
+        }
+
+        public virtual void RemoveZipCode(ZipCode zipCode)
+        {
+
+        }
     }
 }
