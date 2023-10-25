ad5210be7917a1244013eb03acee7966060524d3
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 22:55:03 -0400

    Implmented Reservation

--- Model/Reservation.cs
+++ Model/Reservation.cs
@@ -8,5 +8,32 @@ namespace Model
 {
     public class Reservation
     {
+        public virtual int Id { get; set; }
+        public virtual Movie Movie { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual DateTime ReservationDate { get; set; }
+
+        public Reservation()
+        {
+            
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Reservation comparison = (Reservation)obj;
+                return (Movie.Equals(comparison.Movie) && (Customer.Equals(comparison.Customer)));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
     }
 }
