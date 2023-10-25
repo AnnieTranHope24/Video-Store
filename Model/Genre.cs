09e4666da732cdbb8614d6934694c81a524a5239
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 22:40:25 -0400

    Implemented Genre

--- Model/Genre.cs
+++ Model/Genre.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model
 {
     public class Genre
     {
+        public virtual string Name { get; set; }
+
+        public Genre()
+        {
+            
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Genre comparison = (Genre)obj;
+                return (String.Equals(Name, comparison.Name));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode();
+        }
     }
 }
