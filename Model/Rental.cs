20801f06a3a17427680853f06087436f6abf83a7
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 23:04:42 -0400

    Implemented Rental and added ReturnReceipt

--- Model/Rental.cs
+++ Model/Rental.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model
 {
     public class Rental
     {
+        public virtual int Id { get; set; }
+        public virtual Video Video { get; set; }
+        public virtual Customer Customer { get; set; }
+        public virtual DateTime RentalDate { get; set; }
+        public virtual DateTime DueDate { get; set; }
+        public virtual DateTime? ReturnDate { get; set; }
+        public virtual Rating Rating { get; set; }
+
+        public Rental()
+        {
+            
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Rental comparison = (Rental)obj;
+                return (Video.Equals(comparison.Video) && Customer.Equals(comparison.Customer) && RentalDate.Equals(comparison.RentalDate));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public virtual ReturnReceipt Return()
+        {
+            return null;
+        }
     }
 }
---
+++ Model/ReturnReceipt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Model
+{
+    public class ReturnReceipt
+    {
+        public virtual Reservation FulfilledReservation { get; set; }
+        public virtual Rental NextRental { get; set; }
+
+        public ReturnReceipt()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                ReturnReceipt comparison = (ReturnReceipt)obj;
+                return (FulfilledReservation.Equals(comparison.FulfilledReservation) && (NextRental.Equals(comparison.NextRental)));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
