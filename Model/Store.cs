36717e87094635d16152ed3cd9b229ded74633b5
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 22:50:54 -0400

    Implemented Store

--- Model/Store.cs
+++ Model/Store.cs
@@ -8,5 +8,55 @@ namespace Model
 {
     public class Store
     {
+        public virtual int Id { get; set; }
+        public virtual string StreetAddress { get; set; }
+        public virtual ZipCode ZipCode { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual IList<Employee> Managers { get; protected internal set; }
+        public virtual IList<Video> Videos { get; protected internal set; }
+
+        public Store()
+        {
+            Managers = new List<Employee>();
+            Videos = new List<Video>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Store comparison = (Store)obj;
+                return (String.Equals(StreetAddress, comparison.StreetAddress) && (ZipCode.Equals(comparison.ZipCode)));
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public virtual void AddManager(Employee employee)
+        {
+
+        }
+
+        public virtual void RemoveManager(Employee employee)
+        {
+
+        }
+
+        public virtual void AddVideo(Video video)
+        {
+
+        }
+
+        public virtual void RemoveManager(Video video)
+        {
+
+        }
     }
 }
