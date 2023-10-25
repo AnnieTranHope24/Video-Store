81162f7b3baac84f5fb008ca916cfe74807156ab
Annie Tran <ngoc.tran@hope.edu>
2023-10-24 21:53:54 -0400

    Implemented Video

--- Model/Video.cs
+++ Model/Video.cs
@@ -8,5 +8,36 @@ namespace Model
 {
     public class Video
     {
+        public virtual int Id { get; set; }
+        public virtual Movie Movie { get; set; }
+
+        public virtual DateTime PurchaseDate { get; set; }
+
+        public virtual bool NewArrival { get; set; }
+
+        public virtual Store Store { get; set; }
+
+        public Video()
+        {
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Video comparison = (Video)obj;
+                return (Id == comparison.Id);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
     }
 }
