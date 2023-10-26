00320bb97b3fa48e30e0cfd0562341b3d2f2ef35
Annie Tran <ngoc.tran@hope.edu>
2023-10-25 22:04:13 -0400

    Implemented Name

--- Model/Name.cs
+++ Model/Name.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model
+﻿namespace Model
 {
     public class Name
     {
+        public virtual string Title { get; set; }
+        public virtual string First { get; set; }
+        public virtual string Middle { get; set; }
+        public virtual string Last { get; set; }
+        public virtual string Suffix { get; set; }
+
+        public Name() { }
+
     }
 }
