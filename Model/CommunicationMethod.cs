beaeb678b906f9c0ee0efa725ea083feb0ea54c1
Annie Tran <ngoc.tran@hope.edu>
2023-10-25 21:57:59 -0400

    Implemented CommunicationMethod

--- Model/CommunicationMethod.cs
+++ Model/CommunicationMethod.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Model
 {
     public class CommunicationMethod
     {
+        public enum TimeUnit
+        {
+            Day,
+            Week,
+            Month,
+            Year
+        }
+
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual int Frequency { get; set; }
+        public virtual TimeUnit Units { get; set; }
+        public virtual ISet<Customer> Customers { get; protected internal set; }
+
+        public CommunicationMethod()
+        {
+            Customers = new HashSet<Customer>();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                CommunicationMethod comparison = (CommunicationMethod)obj;
+                return Name.Equals(comparison.Name);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
     }
 }
