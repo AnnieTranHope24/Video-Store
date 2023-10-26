143d9a1f4604e1ff801d40beaf579e7b379e5075
Annie Tran <ngoc.tran@hope.edu>
2023-10-25 22:27:33 -0400

    Implemented Employee

--- Model/Employee.cs
+++ Model/Employee.cs
@@ -1,12 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Model
 {
     public class Employee
     {
+        public virtual int Id { get; set; }
+        public virtual Name Name { get; set; }
+        public virtual DateTime DateHired { get; set; }
+        public virtual DateTime DateOfBirth { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string Password { get; set; }
+        public virtual Store Store { get; set; }
+        public virtual bool IsManager { get; set; }
+        public virtual Employee Supervisor { get; set; }
+
+        public Employee()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Employee comparison = (Employee)obj;
+                return Name.First.Equals(comparison.Name.First) && Name.Last.Equals(comparison.Name.Last) && DateOfBirth.Equals(comparison.DateOfBirth);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
     }
 }
