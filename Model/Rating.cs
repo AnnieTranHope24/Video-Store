a49494d8f1219f518e3bd51ee22348de1524b621
Annie Tran <ngoc.tran@hope.edu>
2023-10-25 22:42:24 -0400

    Implemented Rating

--- Model/Rating.cs
+++ Model/Rating.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Model
+﻿namespace Model
 {
     public class Rating
     {
+        public virtual int Id { get; set; }
+        public virtual int Score { get; set; }
+        public virtual string Comment { get; set; }
+
+        public Rating()
+        {
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                Rating comparison = (Rating)obj;
+                return Id == comparison.Id;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
     }
 }
