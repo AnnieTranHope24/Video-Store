2a24a4943f48704b1ba527a8fafe7ec7f202bd6a
Jocelyn Martinez <jocelyn.martinez@hope.edu>
2023-11-30 17:26:07 -0500

    Added code if possibility of creating a new table

--- Migrations/M10_ZipCodeArea.cs
+++ Migrations/M10_ZipCodeArea.cs
@@ -12,6 +12,10 @@ namespace Migrations
     {
         public override void Down()
         {
+            // Code to create an entirely different table.
+            // Delete.Table("AreaZipCode")
+            //    .InSchema("videostore");
+
             Delete.ForeignKey("ZipCodeToArea")
                 .OnTable("Area")
                 .InSchema("videostore");
@@ -23,6 +27,21 @@ namespace Migrations
 
         public override void Up()
         {
+            // If we need to create a table
+            //Create.Table("AreaZipCode")
+            //    .InSchema("videostore")
+            //    .WithColumn("Area_Id").AsInt64().NotNullable()
+            //    .WithColumn("ZipCode_Id").AsString(255).NotNullable();
+
+            //Create.ForeignKey("FK_AreaZipCode_Area")
+            //    .FromTable("AreaZipCode").ForeignColumn("ZipCode_Id")
+            //    .ToTable("Area").InSchema("videostore").PrimaryColumn("Id");
+
+            //Create.ForeignKey("FK_AreaZipCode_ZipCode")
+            //    .FromTable("AreaZipCode").ForeignColumn("Area_Id")
+            //    .ToTable("ZipCode").InSchema("videostore").PrimaryColumn("Code");
+
+
             Create.Column("ZipCode_Id")
                 .OnTable("Area")
                 .InSchema("videostore")
